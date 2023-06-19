382107c4902440e8565a772af0010bfe9df046d7
divyakanth14 <93990064+divyakanth14@users.noreply.github.com>
2023-06-19 19:07:25 +0530

    Add files via upload

---
+++ myname.java
@@ -0,0 +1,25 @@
+class sintakai
+{
+	public static void main(String[] args)
+	{
+		long account=24545734672749l;
+		float salary=12.4666736f;
+		float tax=99.4678876445f;
+		float grossSalary=(salary+tax);
+		byte age=25;
+		String bankName="HDFC";
+		char gender='M';
+		String status="Single";
+		boolean statement=true;
+		
+		System.out.println("My name is Divyakanth");
+		System.out.println("Age: "+age);
+		System.out.println("Gender: "+gender);
+		System.out.println("Marritual Status: "+status);
+		System.out.println("Total CTC: "+grossSalary);
+		System.out.println("Account number: "+account);
+		System.out.println("Bank name: "+bankName);
+		System.out.println("Account number "+account+" is in bank: "+bankName);
+	}
+	
+}
